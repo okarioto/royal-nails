7a60d12f50385bd04a723843472e701e3e584987
Rioto Oka <okarioto@gmail.com>
2025-01-20 00:25:06 -0800

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals-tw.css";
+import { Analytics } from "@vercel/analytics/react"
 import Header from "./components/Header.js"
 import Footer from "./components/Footer";
 
@@ -33,6 +34,7 @@ export default function RootLayout({ children }) {
       <body className="flex flex-col min-h-screen">
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
