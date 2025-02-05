ed104eff38c614bc70a0b2f019e7d12dabc948e7
Rioto Oka <okarioto@gmail.com>
2025-02-04 17:38:52 -0800

    take off opacity for all photos

--- components/Services.js
+++ components/Services.js
@@ -41,13 +41,13 @@ export default function Services() {
                         <p className="w-[90%] font-subtitle text-secondary-text text-[10px] tracking-tighter text-center md:text-[14px] mb-10">please be aware prices are subject to change depending on complexity</p>
                     </div>
                     <div className="flex flex-col md:flex-row xl:flex-col items-center justify-evenly col-span-2 ">
-                        <img ref={img1Ref} src="/images/threeLotion.jpg" alt="" className="mt-10 mb-10 w-[20rem] md:w-[20rem] md:mb-0 md:mt-0.5 md:h-[25rem] xl:h-auto xl:w-[30rem] xl:mt-10 duration-700 z-10"
+                        <img ref={img1Ref} src="/images/threeLotion.jpg" alt="" className="mt-10 mb-10 w-[20rem] md:w-[20rem] md:mb-0 md:mt-0.5 md:h-[25rem] xl:h-auto xl:w-[30rem] xl:mt-10 duration-700"
                         style={{borderRadius: img1IsInView? "5%":"30%",
-                            opacity: img1IsInView ? "1" : "0.4"
+
                         }}/>
                         <img ref={img2Ref} src="/images/creamSmear2.jpg" alt="" className="w-[20rem] md:w-[20rem] md:h-[25rem] xl:h-auto xl:w-[30rem] duration-700 object-contain"
                             style={{ borderRadius: img2IsInView ? "5%" : "30%",
-                                opacity: img2IsInView ? "1" : "0.4"
+
                             }} />
                     </div>
                 </div>
