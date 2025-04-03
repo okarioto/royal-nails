b8bfa20917457b72b33cd1c7d6572e2175a7e51e
Rioto Oka <okarioto@gmail.com>
2025-04-02 22:22:41 -0700

    fix description and pricing

--- components/bits_and_bots/HoursCard.js
+++ components/bits_and_bots/HoursCard.js
@@ -26,19 +26,19 @@ export default function HoursCard(){
                 <h3 className="font-title tracking-[0.5rem] text-[26px] md:text-[34px] md:mt-7">Hours</h3>
 
                 <div className="flex justify-between w-full mb-2 md:mb-10 md:mt-3">
-                    <h4 className="font-subtitle text-[16px] md:text-[20px]">Weekdays </h4>
-                    <h4 className="font-subtitle text-[14px] md:text-[20px] md:hidden">10AM - 7:30PM </h4>
-                    <h4 className="hidden font-subtitle text-[18px] md:text-[20px] md:block">10:00AM - 07:30PM </h4>
+                    <h4 className="font-subtitle text-[14px] md:text-[20px]">Weekdays </h4>
+                    <h4 className="font-subtitle text-[14px] md:text-[20px] md:hidden">10AM - 7PM </h4>
+                    <h4 className="hidden font-subtitle text-[18px] md:text-[20px] md:block">10:00AM - 07:00PM </h4>
                 </div>
 
                 <div className="flex justify-between w-full mt-2 mb-2 md:mb-10">
-                    <h4 className="font-subtitle text-[16px] md:text-[20px]">Saturday </h4>
+                    <h4 className="font-subtitle text-[14px] md:text-[20px]">Saturday </h4>
                     <h4 className="font-subtitle text-[14px] md:text-[20px] md:hidden">10AM - 6PM </h4>
                     <h4 className="hidden font-subtitle text-[18px] md:text-[20px] md:block">10:00AM - 06:00PM </h4>
                 </div>
 
                 <div className="flex justify-between w-full mt-2 md:mb-10">
-                    <h4 className="font-subtitle tracking-tighter text-[16px] md:text-[20px]">Sunday & Holidays </h4>
+                    <h4 className="font-subtitle tracking-tighter text-[14px] md:text-[20px]">Sunday & Holidays </h4>
                     <h4 className="font-subtitle text-[14px] md:text-[20px] md:hidden">11AM - 6PM </h4>
                     <h4 className="hidden font-subtitle text-[18px] md:text-[20px] md:block">11:00AM - 06:00PM </h4>
                 </div>
